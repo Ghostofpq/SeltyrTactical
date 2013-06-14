70a638569306fc828e312581d54b51ee26dbc043
Ghostofpq <ghostofpq@gmail.com>
2013-06-14 16:50:14 +0200

    euh... je sais pas du tout

--- src/com/ghostofpq/seltyrtactical/main/entities/Player.java
+++ src/com/ghostofpq/seltyrtactical/main/entities/Player.java
@@ -1,15 +1,19 @@
 package com.ghostofpq.seltyrtactical.main.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
 /**
  * Created with IntelliJ IDEA.
- * User: VMPX4526
+ * User: Ghostofpq
  * Date: 12/06/13
  * Time: 14:18
  * To change this template use File | Settings | File Templates.
  */
-
+@Getter
+@Setter
 public class Player {
 
     private String pseudo;
--- src/com/ghostofpq/seltyrtactical/main/entities/Team.java
+++ src/com/ghostofpq/seltyrtactical/main/entities/Team.java
@@ -1,5 +1,8 @@
 package com.ghostofpq.seltyrtactical.main.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
 /**
@@ -9,6 +12,8 @@ import java.util.List;
  * Time: 13:25
  * To change this template use File | Settings | File Templates.
  */
+@Getter
+@Setter
 public class Team {
     private String name;
     private List<Character> team;
