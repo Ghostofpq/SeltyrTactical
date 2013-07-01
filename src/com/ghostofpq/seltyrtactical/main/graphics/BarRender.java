7e712786ca72ec6d54ff9d9194dfc8c0aa1d9bf4
Ghostofpq <ghostofpq@gmail.com>
2013-07-01 13:27:19 +0200

    il faut regarder AngelcodeFont apparement

---
+++ src/com/ghostofpq/seltyrtactical/main/graphics/BarRender.java
@@ -0,0 +1,70 @@
+package com.ghostofpq.seltyrtactical.main.graphics;
+
+import com.ghostofpq.seltyrtactical.main.utils.FontManager;
+import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.UnicodeFont;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: VMPX4526
+ * Date: 01/07/13
+ * Time: 12:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class BarRender {
+    Color colorBack;
+    Color color;
+    float posX1;
+    float posY1;
+    float posX2;
+    float posY2;
+    float posX3;
+    float posY3;
+    float posX4;
+    float posY4;
+    String text;
+    private UnicodeFont font;
+
+    public BarRender(float value, float maxValue, float posX, float posY, float width, float height, Color color, Color colorBack) {
+        this.posX1 = posX;
+        this.posY1 = posY;
+        this.posX2 = posX + width;
+        this.posY2 = posY + height;
+        this.posX3 = posX + (width * (value / maxValue));
+        this.posY3 = posY + height;
+
+        this.font = FontManager.getInstance().getFont();
+        StringBuilder builder = new StringBuilder();
+        builder.append((int) value);
+        builder.append(" / ");
+        builder.append((int) maxValue);
+        text = builder.toString();
+        this.posX4 = posX + ((width - font.getWidth(text)) / 2);
+        this.posY4 = posY + ((height - font.getHeight(text)) / 2);
+        this.color = color;
+        this.colorBack = colorBack;
+    }
+
+    public void render() {
+        GL11.glColor4f(colorBack.getRed(), colorBack.getGreen(), colorBack.getBlue(), 1f);
+
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glVertex2f(posX1, posY1);
+        GL11.glVertex2f(posX2, posY1);
+        GL11.glVertex2f(posX2, posY2);
+        GL11.glVertex2f(posX1, posY2);
+        GL11.glEnd();
+
+        GL11.glColor4f(color.getRed(), color.getGreen(), color.getBlue(), 1f);
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glVertex2f(posX1, posY1);
+        GL11.glVertex2f(posX3, posY1);
+        GL11.glVertex2f(posX3, posY3);
+        GL11.glVertex2f(posX1, posY3);
+        GL11.glEnd();
+
+        font.drawString(posX4, posY4, text,Color.black);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+    }
+}
--- src/com/ghostofpq/seltyrtactical/main/scenes/PlayerMenu.java
+++ src/com/ghostofpq/seltyrtactical/main/scenes/PlayerMenu.java
@@ -2,6 +2,7 @@ package com.ghostofpq.seltyrtactical.main.scenes;
 
 import com.ghostofpq.seltyrtactical.main.Game;
 import com.ghostofpq.seltyrtactical.main.entities.Player;
+import com.ghostofpq.seltyrtactical.main.graphics.BarRender;
 import com.ghostofpq.seltyrtactical.main.graphics.CharacterRender;
 import com.ghostofpq.seltyrtactical.main.graphics.Toolbox;
 import org.newdawn.slick.Color;
@@ -18,6 +19,10 @@ public class PlayerMenu implements Scene {
     private static PlayerMenu instance = new PlayerMenu();
     private Player player;
     private CharacterRender characterRender;
+    private BarRender experienceBar;
+
+    private PlayerMenu() {
+    }
 
     public static PlayerMenu getInstance() {
         if (instance == null) {
@@ -31,13 +36,11 @@ public class PlayerMenu implements Scene {
         return instance;
     }
 
-    private PlayerMenu() {
-    }
-
     @Override
     public void init() {
         player = Game.getInstance().getPlayer();
         characterRender = new CharacterRender(50f, 50f, player.getTeam().getTeam().get(0));
+        experienceBar = new BarRender(200, 250, 100, 50, 100, 20, Color.cyan, Color.magenta);
     }
 
     @Override
@@ -47,7 +50,7 @@ public class PlayerMenu implements Scene {
 
     @Override
     public void render() {
-        Toolbox.drawFrame(10, 10, 400f, 200f, 20f,Color.white);
+        experienceBar.render();
     }
 
     @Override
