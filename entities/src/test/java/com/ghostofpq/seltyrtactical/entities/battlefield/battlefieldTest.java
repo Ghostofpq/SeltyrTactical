bd95012aa2528c89d2b30cb8bd886f70521f556d
Ghostofpq <ghostofpq@gmail.com>
2013-09-02 20:00:17 +0200

    maj des poms

---
+++ entities/src/test/java/com/ghostofpq/seltyrtactical/entities/battlefield/battlefieldTest.java
@@ -0,0 +1,15 @@
+package com.ghostofpq.seltyrtactical.entities.battlefield;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@Slf4j
+@RunWith(JUnit4.class)
+public class battlefieldTest {
+    @Test
+    public void thisAlwaysPasses() {
+        log.debug("Test pass");
+    }
+}
