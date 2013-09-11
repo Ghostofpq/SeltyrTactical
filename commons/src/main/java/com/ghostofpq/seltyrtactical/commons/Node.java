fd2ad69422bf595b77a34449949f3c7d777de1b3
Ghostofpq <ghostofpq@gmail.com>
2013-09-11 18:46:34 +0200

    correction sort de l'arbre

--- commons/src/main/java/com/ghostofpq/seltyrtactical/commons/Node.java
+++ commons/src/main/java/com/ghostofpq/seltyrtactical/commons/Node.java
@@ -51,12 +51,9 @@ public class Node<T> implements Comparable<Node<T>> {
         List<Node<T>> result = new ArrayList<Node<T>>();
         if (getData().equals(element)) {
             result.add(this);
-        } else {
-            if (!getChildren().isEmpty()) {
-                for (Node<T> child : getChildren()) {
-                    result.addAll(child.find(element));
-                }
-            }
+        }
+        for (Node<T> child : getChildren()) {
+            result.addAll(child.find(element));
         }
         return result;
     }
@@ -143,9 +140,9 @@ public class Node<T> implements Comparable<Node<T>> {
     @Override
     public int compareTo(Node<T> other) {
         int res;
-        if (getDistanceFromTop() > other.getDistanceFromTop()) {
+        if (getDistanceFromTop() < other.getDistanceFromTop()) {
             res = -1;
-        } else if (getDistanceFromTop() < other.getDistanceFromTop()) {
+        } else if (getDistanceFromTop() > other.getDistanceFromTop()) {
             res = 1;
         } else {
             res = 0;
